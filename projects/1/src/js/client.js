1275c8f90527e4ebdcde666c99373f459c3fab95
Ekaterina Kashchenko <katikate123@gmail.com>
2016-11-24 10:25:35 +0200

    first

---
+++ projects/1/src/js/client.js
@@ -0,0 +1,39 @@
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+
+const app = document.getElementById('app');
+const CounterComponent = (props) =>
+  <div>
+    {
+      Array.from({ length: props.repeat }).map((item, index) =>
+        <h1 key={index}>{props.counter}</h1>
+      )
+    }
+  </div>
+;
+
+class MainContainer extends Component {
+  constructor(...args) {
+    super(...args);
+    this.state = {repeatCount: 1}
+    console.log(this)
+  }
+
+  changeField (event) {
+    console.log(this)
+    this.setState({repeatCount: event.target.value})
+
+  }
+
+  render() {
+    return (
+    <div>
+      <h1>Hello React</h1>
+      <input type='number' value={this.state.repeatCount} onChange={this.changeField} />
+      <CounterComponent counter={3} repeat={this.state.repeatCount} />,
+      {() => {console.log(this)}}
+    </div>
+   )
+  }
+}
+ReactDOM.render(<MainContainer />, app);
